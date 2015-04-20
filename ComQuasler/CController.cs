4b4f73e8eba10a113128c605cc5d37077d9b3965
GrimbixCode <david@grimbichler.ch>
2015-04-20 16:51:03 +0200

    Create GUI
    Create GUI and a piece of MVC

---
+++ ComQuasler/CController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComQuasler
+{
+    class CController
+    {
+        /// <summary>
+        /// Initialize components for MVC
+        /// </summary>
+        private MainWindow myView;
+        private CModel myModel;
+
+        /// <summary>
+        /// Constructor of class cotroller
+        /// </summary>
+        /// <param name="myModel"></param>
+        public CController(MainWindow myView, CModel myModel)
+        {
+            this.myView = myView;
+            this.myModel = new CModel(myView);
+        }
+
+
+    }
+}
---
+++ ComQuasler/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+
+namespace ComQuasler
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// Uses as View
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initialize components for MVC
+        /// </summary>
+        private CController myController;
+        private CModel myModel;
+
+        /// <summary>
+        /// Constructor of class MainWindow
+        /// </summary>
+        public MainWindow()
+        {
+            myModel = new CModel(this);
+            this.myController = new CController(this, myModel);
+            
+            InitializeComponent();
+        }
+
+
+    }
+}
