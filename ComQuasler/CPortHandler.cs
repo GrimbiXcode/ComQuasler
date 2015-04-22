07f4cbfaa5178e116b62626f0b78838a43aab38e
GrimbixCode <david@grimbichler.ch>
2015-04-22 14:17:14 +0200

    GUI - Port settings
    - Renew GUI
    -Add settings for Port-Properties
    - Create Class CPortHandler
    - Create Class CCompiler

---
+++ ComQuasler/CPortHandler.cs
@@ -0,0 +1,145 @@
+﻿/*****************************************************************************
+ * NAMESPACES
+ *****************************************************************************/
+// Standart namespaces
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+// namespaces for port-communication
+using System.IO.Ports;
+using System.IO;
+/*****************************************************************************/
+
+/*****************************************************************************
+ * Class
+ *****************************************************************************/  
+namespace ComQuasler
+{
+    class CPortHandler
+    {
+        #region Initialize
+        /// <summary>
+        /// Initialize components
+        /// </summary>
+        public SerialPort serial = new SerialPort();                            // creates new serial port
+        public string[] AvailableCOMPorts;                                      // create string array - later, here are all the com ports listed
+        public int SelectedCOMPort;                                             // the number of the current port will be stored in here
+        #endregion Initialize
+
+        // all methodes, which work with the COM port
+        #region COM
+        /// <summary>
+        /// sets up serial port
+        /// </summary>
+        public void SetUpPort()
+        {
+            // setup:
+            serial.BaudRate = 38400;
+            serial.Handshake = System.IO.Ports.Handshake.None;
+            serial.Parity = Parity.None;
+            serial.DataBits = 8;
+            serial.StopBits = StopBits.One;
+            serial.ReadTimeout = 150;
+            serial.WriteTimeout = 50;
+
+            AvailableCOMPorts = SerialPort.GetPortNames();                      // save all available ports in a string array
+            SelectedCOMPort = 0;                                                // which port of them above is selected (start with 0)
+        }
+
+        /// <summary>
+        /// opens the selected com port
+        /// </summary>
+        /// <returns>true = open successfully</returns>
+        bool ConnectPort()
+        {
+            DisconnectPort();                                                   // before open new port, ensure that all ports are closed
+
+            serial.PortName = AvailableCOMPorts[SelectedCOMPort];               // set com port, which should be open
+            try
+            {
+                serial.Open();                                                  // try to open serial port
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// ensure that the com port is closed
+        /// </summary>
+        void DisconnectPort()
+        {
+            if (serial.IsOpen)                                                  // if the port is open...
+            {
+                try
+                {
+                    serial.Close();                                             // ...try to close it
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        /// <summary>
+        /// go through all ports end send a FIND
+        /// </summary>
+        /// <returns>false, if there no ports left</returns>
+        public bool ConnectTestDevice()
+        {
+            //TelegramControlRxTxCounter = 0;                                     // Reset RxTx-Control Counter
+            //if (SelectedCOMPort < AvailableCOMPorts.Length)                     // if ports left
+            //{
+            //    if (ConnectPort()) TelegramSendCMD = TelegramCommands.FIND;     // open port and send FIND telegram
+            //    window.UpdateTBO(window._tboAnschluss, AvailableCOMPorts[SelectedCOMPort]);// updates GUI
+            //    SelectedCOMPort++;                                              // increase, next time, next port will be open
+            //    return true;
+            //}
+            //else                                                                // if no ports left
+            //{
+            //    DisconnectDevice();
+            //    return false;
+            //}
+            return true;
+        }
+
+        /// <summary>
+        /// checks if telegrams have been lost
+        /// </summary>
+        /// <returns>true = not more than 3 telegrams lost (else it causes ConnectionState = NONE and close Port)</returns>
+        public bool CheckConnection()
+        {
+            //if (TelegramControlRxTxCounter > 2)                                 // 3x send a Telegram without response
+            //{
+            //    DisconnectDevice();
+            //    return false;
+            //}
+            return true;
+        }
+
+        /// <summary>
+        /// close com port and set connectionstate to NONE
+        /// </summary>
+        public void DisconnectDevice()
+        {
+            DisconnectPort();                                                   // close com port
+            //ConnectionState = ConnectionStates.NONE;                            // set connection state to NONE
+        }
+
+        #endregion COM
+    }
+}
+/*****************************************************************************/
--- ComQuasler/MainWindow.xaml.cs
+++ ComQuasler/MainWindow.xaml.cs
@@ -1,4 +1,36 @@
-﻿using System;
+﻿/*****************************************************************************
+ * Company:             GrimbixCode
+ *
+ * Project:             Richard AG RM580
+ *
+ * Target:              Windows
+ *                   
+ * Type:                C#-Class
+ *
+ * Description:         Intelligent communication over com-port
+ *
+ * Compiler:            C#
+ *
+ * Filename:            MainWindow.xaml.cs
+ *
+ * Version:             0.1
+ *
+ * Author:              GrimbixCode 
+ *
+ * Creation-Date:       20.04.2015
+ *
+ *****************************************************************************
+ * Modification History:
+ * 
+ * [0.1]    16.04.2010  GrimbixCode     first release
+ *
+ *****************************************************************************/
+
+/*****************************************************************************
+ * NAMESPACES
+ *****************************************************************************/
+// Standart namespaces
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,8 +44,16 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Collections.Generic;
+// namespaces for port-communication
+using System.IO.Ports;
+using System.IO;
+// namespaces for ReadOnlyCollection
+using System.Collections.ObjectModel;
+/*****************************************************************************/
 
+/*****************************************************************************
+ * Class
+ *****************************************************************************/   
 namespace ComQuasler
 {
     /// <summary>
@@ -22,23 +62,55 @@ namespace ComQuasler
     /// </summary>
     public partial class MainWindow : Window
     {
-        /// <summary>
-        /// Initialize components for MVC
-        /// </summary>
-        private CController myController;
-        private CModel myModel;
+        #region Define
+        public static readonly IList<String> qcPortState = new ReadOnlyCollection<string>
+        (new List<String> {"No port selected","Port is closed","Try open port","Port open"});
 
+        #endregion Define
+
+        #region Initialize
+        /// <summary>
+        /// Initialize components
+        /// </summary>
+        
+        public enum ConnectionStates                                            // define all the connection states
+        {
+            NONE, CLOSED, TRYOPEN, CLOSEPORT, OPEN
+        };
+
+        public enum qcStates                                                  // define all the Teststates
+        {
+            NONE, READY, VCC, SYNC, SWITCH_1, SWITCH_2, SWITCH_3, SWITCH_4, SWITCH_5, CONNECT_RM, SW_SWITCH, DISCONNECT_RM, BREAK, CLOSE_CONNECTION, CONFIG, FINISH_TEST
+        };
+
+        private CPortHandler myComPort;
+
+        
+        #endregion Initialize
+
+        #region Constructor
         /// <summary>
         /// Constructor of class MainWindow
         /// </summary>
         public MainWindow()
         {
-            myModel = new CModel(this);
-            this.myController = new CController(this, myModel);
-            
             InitializeComponent();
+            myComPort = new CPortHandler();
+
+            _cboHandshake.ItemsSource = Enum.GetValues(typeof(Handshake));
+            _cboParity.ItemsSource = Enum.GetValues(typeof(Parity));
+            _cboStopBits.ItemsSource = Enum.GetValues(typeof(StopBits));
+            _cboComPort.ItemsSource = SerialPort.GetPortNames();
+            _tboState.Text = qcPortState[0];
         }
 
+        #endregion #Constructor
+
+        #region GUI-Interface
+
+
+        #endregion GUI-Interface
 
     }
 }
+/*****************************************************************************/
